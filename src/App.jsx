6f4dd33b5ae407d0202f42f2d283787ebda06ccb
ChienPM-27 <pminhchien2006@gmail.com>
2025-08-21 15:26:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <main className="pt-20">
+      </main>
+    </div>
+  );
+};
+
+export default App;
